28a1821a8bb6bda0d7225e2dde8a535317291468
hideyuki-hori <montecarlo1101@gmail.com>
2024-07-07 09:34:57 +0900

    style: configure initial project styling for Vite app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from '~/app'
+import '~/app/styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const element = document.querySelector('.love-pmnd')!
+const root = createRoot(element)
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>
 )
